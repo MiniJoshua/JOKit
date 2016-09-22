3c17007e20683d913e49af4693f26b6997b373fb
Joshua <505398605@qq.com>
2016-09-22 11:20:20 +0800

    给UIView+AutoLayout增加一套新的不需要Block回调的方法

--- JOKit/JOKit/Utility/AutoLayout/UIView+JOAutolayout.h
+++ JOKit/JOKit/Utility/AutoLayout/UIView+JOAutolayout.h
@@ -14,53 +14,102 @@ typedef void(^JOViewLayoutBlock) (JOLayoutItem *layoutItem);
 
 @interface UIView(JOAutolayout)
 
-//左边的约束相关.
+#pragma mark - 左边的约束相关
+#pragma mark -
+
+- (void)layoutLeft:(CGFloat)distance;
+- (void)layoutLeftView:(UIView *)leftView distance:(CGFloat)distance;
+- (void)layoutLeftXView:(UIView *)leftView distance:(CGFloat)distance;
 - (void)layoutLeft:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutLeftView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutLeftXView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutLeftView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;    //与给定视图的左边的间距大小的约束
+- (void)layoutLeftXView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;   //与给定视图左对齐的约束
 
-//右边的约束相关.
+#pragma mark - 右边的约束相关
+#pragma mark -
+
+- (void)layoutRight:(CGFloat)distance;
+- (void)layoutRightView:(UIView *)rightView distance:(CGFloat)distance;
+- (void)layoutRightXView:(UIView *)rightView distance:(CGFloat)distance;
 - (void)layoutRight:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutRightView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutRightXView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutRightView:(UIView *)rightView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图的右边的间距大小的约束
+- (void)layoutRightXView:(UIView *)rightView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block; //与给定视图右对齐的约束
 
-//头部的约束相关.
+#pragma mark - 头部的约束相关
+#pragma mark -
+
+- (void)layoutTop:(CGFloat)distance;
+- (void)layoutTopView:(UIView *)topView distance:(CGFloat)distance;
+- (void)layoutTopYView:(UIView *)topView distance:(CGFloat)distance;
 - (void)layoutTop:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutTopView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutTopYView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutTopView:(UIView *)topView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图的上边的间距大小的约束
+- (void)layoutTopYView:(UIView *)topView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block; //与给定视图上对齐的约束
 
-//底部的约束相关
+#pragma mark - 底部的约束相关
+#pragma mark -
+
+- (void)layoutBottom:(CGFloat)distance;
+- (void)layoutBottomView:(UIView *)bottomView distance:(CGFloat)distance;
+- (void)layoutBottomYView:(UIView *)bottomView distance:(CGFloat)distance;
 - (void)layoutBottom:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutBottomView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutBottomYView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutBottomView:(UIView *)bottomView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;    //与给定视图的下边的间距大小的约束
+- (void)layoutBottomYView:(UIView *)bottomView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block;   //与给定视图下对齐的约束
 
-//宽度的约束相关.
+#pragma mark - 宽度的约束相关
+#pragma mark -
+
+- (void)layoutWidth:(CGFloat)width;
+- (void)layoutWidthView:(UIView *)widthView ratio:(CGFloat)ratio;
 - (void)layoutWidth:(CGFloat)width layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutWidthView:(UIView *)widthView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutWidthView:(UIView *)widthView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;    //与给定视图宽度比例大小的约束
 
-//高度的约束相关.
+#pragma mark - 高度的约束相关
+#pragma mark -
+
+- (void)layoutHeight:(CGFloat)height;
+- (void)layoutHeightView:(UIView *)heightView ratio:(CGFloat)ratio;
 - (void)layoutHeight:(CGFloat)height layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutHeightView:(UIView *)heightView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutHeightView:(UIView *)heightView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图高度比例大小的约束
 
-//中心的约束相关.
-- (void)layoutCenterXView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutCenterYView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutCenterView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block;
+#pragma mark - 中心的约束相关
+#pragma mark -
 
-//宽高的约束相关.
-- (void)layoutWidthHeightRatio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutHeightWidthRatio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutWidthHeightView:(UIView *)heightView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutHeightWidthView:(UIView *)widthView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutCenterXView:(UIView *)centerView;
+- (void)layoutCenterYView:(UIView *)centerView;
+- (void)layoutCenterView:(UIView *)centerView;
+- (void)layoutCenterXView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图中心点x的相同的约束
+- (void)layoutCenterYView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图中心点y的相同的约束
+- (void)layoutCenterView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block;   //与给定视图中心点的相同的约束
 
-//四周的约束相关.
+#pragma mark - 宽高的约束相关.
+#pragma mark -
+
+- (void)layoutWidthHeightRatio:(CGFloat)ratio;
+- (void)layoutHeightWidthRatio:(CGFloat)ratio;
+- (void)layoutWidthHeightView:(UIView *)heightView ratio:(CGFloat)ratio;
+- (void)layoutHeightWidthView:(UIView *)widthView ratio:(CGFloat)ratio;
+- (void)layoutWidthHeightRatio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;   //宽高比的约束
+- (void)layoutHeightWidthRatio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;   //高宽币的约束
+- (void)layoutWidthHeightView:(UIView *)heightView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block; //与给定视图的宽高比的约束
+- (void)layoutHeightWidthView:(UIView *)widthView ratio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图的高宽比的约束
+
+#pragma mark - 四周的约束相关.
+#pragma mark -
+
+- (void)layoutEdge:(UIEdgeInsets)edge;
 - (void)layoutEdge:(UIEdgeInsets)edge layoutItemHandler:(JOViewLayoutBlock)block;
 
-//size的约束相关.
-- (void)layoutSize:(CGSize)size layoutItemHandler:(JOViewLayoutBlock)block;
-- (void)layoutSizeView:(UIView *)sizeView layoutItemHandler:(JOViewLayoutBlock)block;
+#pragma mark - size的约束相关.
+#pragma mark -
 
-//same的约束相关.
-- (void)layoutSameView:(UIView *)sameView layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutSize:(CGSize)size;
+- (void)layoutSizeView:(UIView *)sizeView;
+- (void)layoutSize:(CGSize)size layoutItemHandler:(JOViewLayoutBlock)block;
+- (void)layoutSizeView:(UIView *)sizeView layoutItemHandler:(JOViewLayoutBlock)block;  //与给定视图的Size的约束
+
+#pragma mark - same的约束相关.
+#pragma mark -
+
+- (void)layoutSameView:(UIView *)sameView;
+- (void)layoutSameView:(UIView *)sameView layoutItemHandler:(JOViewLayoutBlock)block;   //与给定视图相同的约束
 
 @end
--- JOKit/JOKit/Utility/AutoLayout/UIView+JOAutolayout.m
+++ JOKit/JOKit/Utility/AutoLayout/UIView+JOAutolayout.m
@@ -18,6 +18,21 @@
     [JOLayout layoutWithItem:item];
 }
 
+#pragma mark - 左边的约束相关
+#pragma mark -
+
+- (void)layoutLeft:(CGFloat)distance {
+    [self layoutLeft:distance layoutItemHandler:nil];
+}
+
+- (void)layoutLeftView:(UIView *)leftView distance:(CGFloat)distance {
+    [self layoutLeftView:leftView distance:distance layoutItemHandler:nil];
+}
+
+- (void)layoutLeftXView:(UIView *)leftView distance:(CGFloat)distance {
+    [self layoutLeftXView:leftView distance:distance layoutItemHandler:nil];
+}
+
 - (void)layoutLeft:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
     JOLayoutItem *item = [JOLayoutItem layoutLeftDistance:distance view:self];
@@ -36,23 +51,53 @@
     [self layoutWithItem:item itemBlock:block];
 };
 
+#pragma mark - 右边的约束相关
+#pragma mark -
+
+- (void)layoutRight:(CGFloat)distance {
+    [self layoutRight:distance layoutItemHandler:nil];
+}
+
+- (void)layoutRightView:(UIView *)rightView distance:(CGFloat)distance {
+    [self layoutRightView:rightView distance:distance layoutItemHandler:nil];
+}
+
+- (void)layoutRightXView:(UIView *)rightView distance:(CGFloat)distance {
+    [self layoutRightXView:rightView distance:distance layoutItemHandler:nil];
+}
+
 - (void)layoutRight:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
     JOLayoutItem *item = [JOLayoutItem layoutRightDistance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
 
-- (void)layoutRightView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
+- (void)layoutRightView:(UIView *)rightView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
-    JOLayoutItem *item = [JOLayoutItem layoutRightView:leftView distance:distance view:self];
+    JOLayoutItem *item = [JOLayoutItem layoutRightView:rightView distance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
 
-- (void)layoutRightXView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
+- (void)layoutRightXView:(UIView *)rightView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
-    JOLayoutItem *item = [JOLayoutItem layoutRightXView:leftView distance:distance view:self];
+    JOLayoutItem *item = [JOLayoutItem layoutRightXView:rightView distance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
+
+#pragma mark - 头部的约束相关
+#pragma mark -
+
+- (void)layoutTop:(CGFloat)distance {
+    [self layoutTop:distance layoutItemHandler:nil];
+}
+
+- (void)layoutTopView:(UIView *)topView distance:(CGFloat)distance {
+    [self layoutTopView:topView distance:distance layoutItemHandler:nil];
+}
+
+- (void)layoutTopYView:(UIView *)topView distance:(CGFloat)distance {
+    [self layoutTopYView:topView distance:distance layoutItemHandler:nil];
+}
 
 - (void)layoutTop:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
@@ -60,17 +105,32 @@
     [self layoutWithItem:item itemBlock:block];
 };
 
-- (void)layoutTopView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
+- (void)layoutTopView:(UIView *)topView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
-    JOLayoutItem *item = [JOLayoutItem layoutTopView:leftView distance:distance view:self];
+    JOLayoutItem *item = [JOLayoutItem layoutTopView:topView distance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
 
-- (void)layoutTopYView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
+- (void)layoutTopYView:(UIView *)topView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
-    JOLayoutItem *item = [JOLayoutItem layoutTopYView:leftView distance:distance view:self];
+    JOLayoutItem *item = [JOLayoutItem layoutTopYView:topView distance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
+
+#pragma mark - 底部的约束相关
+#pragma mark -
+
+- (void)layoutBottom:(CGFloat)distance {
+    [self layoutBottom:distance layoutItemHandler:nil];
+}
+
+- (void)layoutBottomView:(UIView *)bottomView distance:(CGFloat)distance {
+    [self layoutBottomView:bottomView distance:distance layoutItemHandler:nil];
+}
+
+- (void)layoutBottomYView:(UIView *)bottomView distance:(CGFloat)distance {
+    [self layoutBottomYView:bottomView distance:distance layoutItemHandler:nil];
+}
 
 - (void)layoutBottom:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
@@ -78,17 +138,28 @@
     [self layoutWithItem:item itemBlock:block];
 };
 
-- (void)layoutBottomView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
+- (void)layoutBottomView:(UIView *)bottomView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
-    JOLayoutItem *item = [JOLayoutItem layoutBottomView:leftView distance:distance view:self];
+    JOLayoutItem *item = [JOLayoutItem layoutBottomView:bottomView distance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
 
-- (void)layoutBottomYView:(UIView *)leftView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
+- (void)layoutBottomYView:(UIView *)bottomView distance:(CGFloat)distance layoutItemHandler:(JOViewLayoutBlock)block {
     
-    JOLayoutItem *item = [JOLayoutItem layoutBottomYView:leftView distance:distance view:self];
+    JOLayoutItem *item = [JOLayoutItem layoutBottomYView:bottomView distance:distance view:self];
     [self layoutWithItem:item itemBlock:block];
 };
+
+#pragma mark - 宽度的约束相关
+#pragma mark -
+
+- (void)layoutWidth:(CGFloat)width {
+    [self layoutWidth:width layoutItemHandler:nil];
+}
+
+- (void)layoutWidthView:(UIView *)widthView ratio:(CGFloat)ratio {
+    [self layoutWidthView:widthView ratio:ratio layoutItemHandler:nil];
+}
 
 - (void)layoutWidth:(CGFloat)width layoutItemHandler:(JOViewLayoutBlock)block {
     
@@ -102,6 +173,17 @@
     [self layoutWithItem:item itemBlock:block];
 };
 
+#pragma mark - 高度的约束相关
+#pragma mark -
+
+- (void)layoutHeight:(CGFloat)height {
+    [self layoutHeight:height layoutItemHandler:nil];
+}
+
+- (void)layoutHeightView:(UIView *)heightView ratio:(CGFloat)ratio {
+    [self layoutHeightView:heightView ratio:ratio layoutItemHandler:nil];
+}
+
 - (void)layoutHeight:(CGFloat)height layoutItemHandler:(JOViewLayoutBlock)block {
     
     JOLayoutItem *item = [JOLayoutItem layoutHeightDistance:height view:self];
@@ -113,6 +195,21 @@
     JOLayoutItem *item = [JOLayoutItem layoutHeightView:heightView ratio:ratio view:self];
     [self layoutWithItem:item itemBlock:block];
 };
+
+#pragma mark - 中心的约束相关
+#pragma mark -
+
+- (void)layoutCenterXView:(UIView *)centerView {
+    [self layoutCenterXView:centerView layoutItemHandler:nil];
+}
+
+- (void)layoutCenterYView:(UIView *)centerView {
+    [self layoutCenterYView:centerView layoutItemHandler:nil];
+}
+
+- (void)layoutCenterView:(UIView *)centerView {
+    [self layoutCenterView:centerView layoutItemHandler:nil];
+}
 
 - (void)layoutCenterXView:(UIView *)centerView layoutItemHandler:(JOViewLayoutBlock)block {
     
@@ -133,6 +230,25 @@
     [self layoutWithItem:item itemBlock:block];
     [self layoutWithItem:item2 itemBlock:block];
 };
+
+#pragma mark - 宽高的约束相关.
+#pragma mark -
+
+- (void)layoutWidthHeightRatio:(CGFloat)ratio {
+    [self layoutWidthHeightRatio:ratio layoutItemHandler:nil];
+}
+
+- (void)layoutHeightWidthRatio:(CGFloat)ratio {
+    [self layoutHeightWidthRatio:ratio layoutItemHandler:nil];
+}
+
+- (void)layoutWidthHeightView:(UIView *)heightView ratio:(CGFloat)ratio {
+    [self layoutWidthHeightView:heightView ratio:ratio layoutItemHandler:nil];
+}
+
+- (void)layoutHeightWidthView:(UIView *)widthView ratio:(CGFloat)ratio {
+    [self layoutHeightWidthView:widthView ratio:ratio layoutItemHandler:nil];
+}
 
 - (void)layoutWidthHeightRatio:(CGFloat)ratio layoutItemHandler:(JOViewLayoutBlock)block {
     
@@ -158,12 +274,30 @@
     [self layoutWithItem:item itemBlock:block];
 };
 
+#pragma mark - 四周的约束相关.
+#pragma mark -
+
+- (void)layoutEdge:(UIEdgeInsets)edge {
+    [self layoutEdge:edge layoutItemHandler:nil];
+}
+
 - (void)layoutEdge:(UIEdgeInsets)edge layoutItemHandler:(JOViewLayoutBlock)block {
     
     [self layoutTop:edge.top layoutItemHandler:block];
     [self layoutLeft:edge.left layoutItemHandler:block];
     [self layoutBottom:edge.bottom layoutItemHandler:block];
     [self layoutRight:edge.right layoutItemHandler:block];
+}
+
+#pragma mark - size的约束相关.
+#pragma mark -
+
+- (void)layoutSize:(CGSize)size {
+    [self layoutSize:size layoutItemHandler:nil];
+}
+
+- (void)layoutSizeView:(UIView *)sizeView {
+    [self layoutSizeView:sizeView layoutItemHandler:nil];
 }
 
 - (void)layoutSize:(CGSize)size layoutItemHandler:(JOViewLayoutBlock)block {
@@ -176,6 +310,14 @@
     
     [self layoutWidthView:sizeView ratio:1. layoutItemHandler:block];
     [self layoutHeightView:sizeView ratio:1. layoutItemHandler:block];
+}
+
+#pragma mark - same的约束相关.
+#pragma mark -
+
+- (void)layoutSameView:(UIView *)sameView {
+
+    [self layoutSameView:sameView layoutItemHandler:nil];
 }
 
 - (void)layoutSameView:(UIView *)sameView layoutItemHandler:(JOViewLayoutBlock)block {

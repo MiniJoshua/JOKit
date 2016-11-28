6cdca8934290bf54b8c48fddc6c09e71bc32e7de
Joshua <505398605@qq.com>
2016-11-28 17:18:47 +0800

    增加基于JOViewController的ViewModel

---
+++ JOKit/JOKit/UIKit/UIViewController/JOViewControllerModel.m
@@ -0,0 +1,15 @@
+//
+//  JOViewControllerModel.m
+//  JOKit
+//
+//  Created by 刘维 on 16/11/16.
+//  Copyright © 2016年 Joshua. All rights reserved.
+//
+
+#import "JOViewControllerModel.h"
+#import "JOLoadingView.h"
+#import "JOKit.h"
+
+@implementation JOViewControllerModel
+
+@end

111e160cda30bcaf51be2848f5ec7900ea052d86
Joshua <505398605@qq.com>
2017-01-11 15:15:38 +0800

    添加NSData的category文件

---
+++ JOKit/JOKit/Extend/NSData+JOExtend.h
@@ -0,0 +1,15 @@
+//
+//  NSData+JOExtend.h
+//  JOKit
+//
+//  Created by 刘维 on 17/1/9.
+//  Copyright © 2017年 Joshua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData(JOExtend)
+
+
+
+@end
---
+++ JOKit/JOKit/Extend/NSData+JOExtend.m
@@ -0,0 +1,13 @@
+//
+//  NSData+JOExtend.m
+//  JOKit
+//
+//  Created by 刘维 on 17/1/9.
+//  Copyright © 2017年 Joshua. All rights reserved.
+//
+
+#import "NSData+JOExtend.h"
+
+@implementation NSData(JOExtend)
+
+@end

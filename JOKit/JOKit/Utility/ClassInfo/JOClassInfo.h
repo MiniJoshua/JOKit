64f451cea6cbbdd45d6b7ffe708e18556848c6fb
Joshua <505398605@qq.com>
2017-01-20 15:08:24 +0800

    完善JOClassInfo的内容

--- JOKit/JOKit/Utility/ClassInfo/JOClassInfo.h
+++ JOKit/JOKit/Utility/ClassInfo/JOClassInfo.h
@@ -6,6 +6,10 @@
 //  Copyright © 2017年 Joshua. All rights reserved.
 //
 
+/*
+ 参考YYKit中的实现
+ */
+
 #import <Foundation/Foundation.h>
 #import "JOMacro.h"
 
@@ -50,26 +54,46 @@ typedef NS_OPTIONS(NSUInteger, JOEncodingType) {
     
     //property的属性
     JOEncodingTypePropertyMask          = 0XFF00,
-    JOEncodingTypePropertyReadonly      = 1 << 8,   //readonly
-    JOEncodingTypePropertyCopy          = 1 << 9,   //copy
-    JOEncodingTypePropertyReatin        = 1 << 10,  //reatin strong
-    JOEncodingTypePropertyNonatomic     = 1 << 11,  //nonatomic
-    JOEncodingTypePropertyCustomGetter  = 1 << 12,  //getter =
-    JOEncodingTypePropertyCustomSetter  = 1 << 13,  //setter =
-    JOEncodingTypePropertyDynamic       = 1 << 14,  //@dynamic
-    JOEncodingTypePropertyWeak          = 1 << 15,  //weak
+    JOEncodingTypePropertyReadonly      = 1 << 8,   //readonly  -------->R
+    JOEncodingTypePropertyCopy          = 1 << 9,   //copy  -------->C
+    JOEncodingTypePropertyReatin        = 1 << 10,  //reatin strong  -------->&
+    JOEncodingTypePropertyNonatomic     = 1 << 11,  //nonatomic  -------->N
+    JOEncodingTypePropertyCustomGetter  = 1 << 12,  //getter = -------->G
+    JOEncodingTypePropertyCustomSetter  = 1 << 13,  //setter = -------->S
+    JOEncodingTypePropertyDynamic       = 1 << 14,  //@dynamic -------->D
+    JOEncodingTypePropertyWeak          = 1 << 15,  //weak  -------->W
     
     JOEncodingTypePropertyStrong        = JOEncodingTypePropertyReatin,
     
-    //函数返回值的限定语
+    
+    /**
+     https://stackoverflow.com/questions/5609564/objective-c-in-out-inout-byref-byval-and-so-on-what-are-they
+     http://www.informit.com/articles/article.aspx?p=1438422&seqNum=2
+     */
+    //函数参数以及返回类型的限定词
     JOEncodingTypeQualifierMask     = 0XFF0000,
-    JOEncodingTypeQualifierConst    = 1 << 16,   //const------>r
-    JOEncodingTypeQualifierIn       = 1 << 17,   //in------>n
-    JOEncodingTypeQualifierInout    = 1 << 18,  //inout------>N
-    JOEncodingTypeQualifierOut      = 1 << 19,  //out------>o
-    JOEncodingTypeQualifierBycopy   = 1 << 20,  //bycopy------>O
-    JOEncodingTypeQualifierByref    = 1 << 21,  //byref------>R
-    JOEncodingTypeQualifierOneway   = 1 << 22,  //oneway------>V
+    JOEncodingTypeQualifierConst    = 1 << 16,   //const------>r  可以针对返回值跟参数
+    /**
+     argument is an input argument only and won’t be referenced later
+     */
+    JOEncodingTypeQualifierIn       = 1 << 17,   //in------>n     仅针对参数  貌似也能对函数返回值做限定 不会报错 也能被解析出来
+    JOEncodingTypeQualifierInout    = 1 << 18,  //inout------>N   仅针对参数 默认的  貌似也能对函数返回值做限定 不会报错 也能被解析出来
+    /**
+     argument is an output argument only, used to return a value by reference
+     */
+    JOEncodingTypeQualifierOut      = 1 << 19,  //out------>o     仅针对参数   貌似也能对函数返回值做限定 不会报错 也能被解析出来
+    
+    
+    JOEncodingTypeQualifierBycopy   = 1 << 20,  //bycopy------>O  针对返回值跟参数
+    /**
+     不是传入地址的约束
+     */
+    JOEncodingTypeQualifierByref    = 1 << 21,  //byref------>R   针对返回值跟参数  默认的
+    
+    /**
+     不需要中断等待这个函数执行完在继续,因为没必要, 如果你想对立面的某种状态进行捕捉的话就不能加该约束
+     */
+    JOEncodingTypeQualifierOneway   = 1 << 22,  //oneway------>V  仅针对返回值 且返回值为void
 };
 
 /**
@@ -82,6 +106,8 @@ typedef NS_OPTIONS(NSUInteger, JOEncodingType) {
  @return JOEncodingType
  */
 JO_EXTERN JOEncodingType JOEncodingGetType(const char *encodingType);
+
+#pragma mark - Ivar
 
 /*--------------------------------------关于Class的相关信息-----------------------------*/
 
@@ -159,6 +185,8 @@ struct objc_ivar ivar_list[1] ;
 
 @end
 
+#pragma mark - Method
+
 /*---------------------Method的相关信息-----------------------*/
 /**
  Method的struct定义
@@ -181,15 +209,15 @@ struct objc_method_list {
 
 @interface JOClassMethodInfo : NSObject
 
-/*Method*/
+/* Method */
 @property (nonatomic, assign, readonly) Method method;
-/*方法名字*/
+/* 方法名字 */
 @property (nonatomic, strong, readonly) NSString *name;
-/*SEL*/
+/* SEL */
 @property (nonatomic, assign, readonly) SEL sel;
-/*IMP*/
+/* IMP */
 @property (nonatomic, assign, readonly) IMP imp;
-/*参数的个数*/
+/* 参数的个数 */
 @property (nonatomic, assign, readonly) size_t argumentCount;
 /** 
  包括返回的类型跟参数的类型.
@@ -200,11 +228,26 @@ struct objc_method_list {
  @16    @参数类型      16:偏移量
  */
 @property (nonatomic, strong, readonly) NSString *typeEncoding;
-/*方法返回的类型*/
+/**方法返回的类型
+ PS:对于 -(byref union JOUnion)byrefFunUnion;  得到的返回类型就直接为R 而非是R(...)
+    对于 -(inout union JOUnion)funUnion; 得到的返回的类型就为正常的 N(...)
+    只要是byref修饰的 都只返回R 后面不会有返回类型,不知道是这个byref的特殊性还是啥原因
+    查了相关byref的资料:这是一个默认的属性 貌似解释的是一个proxy可以在远端被创建.并不是用来限定必须传入对象的引用
+                    你可以通过一个代理事件来获取该对象,而不是非要等到他被加载了之后才行.
+    bycopy的话,说明这个参数必须遵循NSCodying的协议
+    
+    所以在针对byref的特殊情况返回的时候,我会通过typeEncoding中去截取R后面的内容最为retunTypeEncoding的值
+ */
 @property (nonatomic, strong, readonly) NSString *retunTypeEncoding;
 /*方法返回的JOEncodingType包含了返回前面的修饰词(in out inout bycopy oneway...)*/
 @property (nonatomic, assign, readonly) JOEncodingType returnEncodingType;
-/*所有的参数类型字符串的数组*/
+/**
+ 所有的参数类型字符串的数组
+ 第一个为返回的类型
+ 第二个为消息的接受者 id
+ 第三个为消息的SEL
+ 第四个开始才是传入的参数.
+ */
 @property (nonatomic, strong, readonly) NSArray <NSString *> *argumentTypeEncodingArray;
 
 /**
@@ -217,6 +260,110 @@ struct objc_method_list {
 
 @end
 
+#pragma mark - Property
+
+/*---------------------property的相关信息-----------------------*/
+
+/**
+ typedef struct {
+ const char *name;          // The name of the attribute
+ const char *value;          // The value of the attribute (usually empty)
+} objc_property_attribute_t;
+ 
+ objc_class的struct中没有包含objc_property的对象,
+ 猜想其原因应该是:property的属性的变量在编译期间会被转换成set get ivar分别保存,所有没必要再保存吧
+ */
+
+@interface JOClassPropertyInfo : NSObject
+
+@property (nonatomic, assign, readonly) objc_property_t property;
+/* property的名字 */
+@property (nonatomic, strong, readonly) NSString *name;
+/**
+ property对应的ivar的名字,一般默认是在前面加上 _
+ @synthesize 可以使用这个改变property对应的名字
+ */
+@property (nonatomic, strong, readonly) NSString *ivarName;
+/**
+ property的attributes的属性.
+ e.g: T@"JOObject<myProtocol>",R,&,N,V_non_retain_readonly_protocol_object
+ */
+@property (nonatomic, strong, readonly) NSString *propertyAttributes;
+/* property的类型 即上面的T对应的值:@"JOObject<myProtocol>" */
+@property (nonatomic, strong, readonly) NSString *typeEncoding;
+/** 
+ JOEncodingType 包含了所有的property的相关属性以及类型
+ 取类型: encodingType & JOEncodingTypeMask 即可获得类型部分在与具体的类型做&操作 就能得到是否属于具体的类型.
+ 取属性(retain...): encodingType & JOEncodingTypePropertyMask  跟上面的一样的操作
+ */
+@property (nonatomic, assign, readonly) JOEncodingType encodingType;
+/* Class类型 当不是一个object类型的时候为nil*/
+@property (nonatomic, assign, readonly) Class cls;
+/* get方法的SEL */
+@property (nonatomic, assign, readonly) SEL getter;
+/* set方法的SEL */
+@property (nonatomic, assign, readonly) SEL setter;
+/* 所有的protocols 不存在的时候为nil*/
+@property (nonatomic, strong, readonly) NSArray<NSString *> *protocols;
+
+/**
+ 初始化方法
+
+ @param property objc_property_t
+ @return JOClassPropertyInfo 或者 nil
+ */
+- (instancetype)initWithProperty:(objc_property_t)property;
+
+@end
+
+#pragma mark - Class Info
+
 @interface JOClassInfo : NSObject
+
+/* Class */
+@property (nonatomic, assign, readonly) Class cls;
+/* super class */
+@property (nonatomic, assign, readonly) Class superCls;
+/* metaCls */
+@property (nonatomic, assign, readonly) Class metaCls;
+/* 是否是元类 */
+@property (nonatomic, assign, readonly) BOOL isMeta;
+/* 父类的JOClassInfo的对象 */
+@property (nonatomic, strong, readonly) JOClassInfo *superClassInfo;
+/* 类名 */
+@property (nonatomic, strong, readonly) NSString *name;
+/* 类所支持的协议 */
+@property (nonatomic, strong, readonly) NSArray <NSString *> *protocols;
+/* ivar 的字典类型 key为ivar的名字  value为JOClassIvarInfo */
+@property (nonatomic, strong, readonly) NSDictionary <NSString *, JOClassIvarInfo *> *ivarInfos;
+/**
+ method 的字典类型 key为method的名字  value为JOClassMethodInfo 
+ PS:里面会包含所有catgory里面新增的方法.
+ */
+@property (nonatomic, strong, readonly) NSDictionary <NSString *, JOClassMethodInfo *> *methodInfos;
+/* property 的字典类型 key为property的名字  value为JOClassPropertyInfo */
+@property (nonatomic, strong, readonly) NSDictionary <NSString *, JOClassPropertyInfo *> *propertyInfos;
+
+/**
+ 得到一个class的JOClassInfo的对象.
+ 在你第一次调用这个时候或者已经存下在缓存中,你必须确保class没有使用class_addMethod等方法为其添加新的方法或者ivar.
+ 如果更改该过你需要使用下面提供的classInfoUpdateWithClass去获取新的
+
+ @param cls Class.
+ @return JOClassInfo 或者 nil.
+ */
++ (instancetype)classInfoWithClass:(Class)cls;
++ (instancetype)classInfoWithClassName:(NSString *)className;
+
+/**
+ 得到一个class的JOClassInfo的对象.
+ 你的class使用class_addMethod等方法为其添加新的方法或者ivar.需要调用该方法去获取JOClassInfo的对象.
+ 在你没法确定其是否使用那些runtime的方法为其添加过方法或者ivar的时候,建议你也使用该方法去拿JOClassInfo该对象.
+
+ @param cls Class.
+ @return JOClassInfo 或者 nil.
+ */
++ (instancetype)classInfoUpdateWithClass:(Class)cls;
++ (instancetype)classInfoUpdateWithClassName:(NSString *)className;
 
 @end
--- JOKit/JOKit/Utility/ClassInfo/JOClassInfo.m
+++ JOKit/JOKit/Utility/ClassInfo/JOClassInfo.m
@@ -14,7 +14,7 @@ JOEncodingType JOEncodingGetType(const char *encodingType) {
         return JOEncodingTypeUnknown;
     }
     
-    //返回值限定词可以是一个组合: - (in out void)test;   - (bycout out void)test; 
+    //限定词可以是一个组合: - (in out void)test;   - (bycopy out void)test;
     JOEncodingType qualifierType = 0;
     BOOL qualifierEndstate = NO;
     while (!qualifierEndstate) {
@@ -125,9 +125,183 @@ JOEncodingType JOEncodingGetType(const char *encodingType) {
     self = [super init];
     
     if (self) {
+        _method = method;
+        _sel = method_getName(method);
+        _name = NSStringFromSelector(_sel);
+        _imp = method_getImplementation(method);
+        _argumentCount = method_getNumberOfArguments(method);
+        const char *typeEncoding = method_getTypeEncoding(method);
+        if (typeEncoding) {
+            _typeEncoding = [NSString stringWithUTF8String:typeEncoding];
+        }else {
+            _typeEncoding = @"";
+        }
         
+        char *returnTypeEncoding = method_copyReturnType(method);
+        if (returnTypeEncoding) {
+            
+            //如果为R的话 需要从typeEncoding中取对应的类型
+            if (!memcmp(returnTypeEncoding, "R", 1)) {
+                
+                size_t temp = 1;
+                while (*(typeEncoding+temp) < '0' || *(typeEncoding+temp) > '9') {
+                    temp++;
+                }
+                char tempTypeEncoding[temp];
+                memcpy(tempTypeEncoding, typeEncoding, temp);
+                _retunTypeEncoding = [NSString stringWithUTF8String:tempTypeEncoding];
+                
+            }else{
+                _retunTypeEncoding = [NSString stringWithUTF8String:returnTypeEncoding];
+            }
+            
+            free(returnTypeEncoding);
+        }else {
+           _retunTypeEncoding = @"";
+        }
+        
+        _returnEncodingType = JOEncodingGetType(returnTypeEncoding);
+        
+        NSMutableArray *argTypes = [NSMutableArray array];
+        for (int i = 0; i < _argumentCount; i++) {
+            char *argType = method_copyArgumentType(method, i);
+            NSString *type;
+            if (argType) {
+                type = [NSString stringWithUTF8String:argType];
+                free(argType);
+            }else {
+                type = @"";
+            }
+            [argTypes addObject:type];
+        }
+        _argumentTypeEncodingArray = [argTypes copy];
     }
     
+    return self;
+}
+
+@end
+
+#pragma mark - Class Property Info
+
+@implementation JOClassPropertyInfo
+
+- (instancetype)initWithProperty:(objc_property_t)property {
+
+    if (!property) {
+        return nil;
+    }
+    
+    self = [super init];
+    
+    if (self) {
+        _property = property;
+        const char *propertyName = property_getName(property);
+        _name =(propertyName)?[NSString stringWithUTF8String:propertyName]:nil;
+        const char *attribute = property_getAttributes(property);
+        _propertyAttributes = (attribute)?[NSString stringWithUTF8String:attribute]:nil;
+        
+        JOEncodingType type = 0;
+        uint attributeCount;
+        objc_property_attribute_t *attributes = property_copyAttributeList(property, &attributeCount);
+        
+        @JOExitExcute{
+            if (attributes) {
+                free(attributes);
+            }
+        };
+        
+        for (int i = 0; i < attributeCount; i++) {
+            const char *attributeName = attributes[i].name;
+            const char *attributeValue = attributes[i].value;
+            
+            if (!(memcmp(attributeName, "T", 1))) {
+
+                if (attributeValue) {
+                    _typeEncoding = [NSString stringWithUTF8String:attributeValue];
+                    type = JOEncodingGetType(attributeValue);
+                    NSScanner *scanner = [NSScanner scannerWithString:_typeEncoding];
+                    
+                    if ([scanner scanString:@"@\"" intoString:NULL]) {
+                        //获取Class的类型
+                        NSString *className;
+                        if ([scanner scanUpToCharactersFromSet:[NSMutableCharacterSet characterSetWithCharactersInString:@"\"<"] intoString:&className]) {
+                            if ([className length]) {
+                                _cls = NSClassFromString(className);
+                            }
+                        }
+                    }
+                    
+                    //获取protocols
+                    NSMutableArray *protocolArray = [NSMutableArray array];
+                    while ([scanner scanString:@"<" intoString:NULL]) {
+                        NSString *protocol;
+                        if ([scanner scanUpToCharactersFromSet:[NSMutableCharacterSet characterSetWithCharactersInString:@">"] intoString:&protocol]) {
+                            if ([protocol length]) {
+                                [protocolArray addObject:protocol];
+                            }
+                        }
+                        [scanner scanString:@">" intoString:NULL];
+                    }
+                    if ([protocolArray count]) {
+                        _protocols = [protocolArray copy];
+                    }
+                }
+            }else if (!(memcmp(attributeName, "R", 1))) {
+                //readonly
+                type |= JOEncodingTypePropertyReadonly;
+            }else if (!(memcmp(attributeName, "C", 1))) {
+                //copy
+                type |= JOEncodingTypePropertyCopy;
+            }else if (!(memcmp(attributeName, "&", 1))) {
+                //retain
+                type |= JOEncodingTypePropertyReatin;
+            }else if (!(memcmp(attributeName, "N", 1))) {
+                //nonatomic
+                type |= JOEncodingTypePropertyNonatomic;
+            }else if (!(memcmp(attributeName, "G", 1))) {
+                //自定义的getter方法
+                type |= JOEncodingTypePropertyCustomGetter;
+                
+                if (attributeValue) {
+                    _getter = NSSelectorFromString([NSString stringWithUTF8String:attributeValue]);
+                }
+                
+            }else if (!(memcmp(attributeName, "S", 1))) {
+                //自定义的setter方法
+                type |= JOEncodingTypePropertyCustomSetter;
+                
+                if (attributeValue) {
+                    _setter = NSSelectorFromString([NSString stringWithUTF8String:attributeValue]);
+                }
+                
+            }else if (!(memcmp(attributeName, "D", 1))) {
+                //@dynamic
+                type |= JOEncodingTypePropertyDynamic;
+            }else if (!(memcmp(attributeName, "W", 1))) {
+                //weak
+                type |= JOEncodingTypePropertyWeak;
+            }else if (!(memcmp(attributeName, "V", 1))) {
+                //ivar name
+                if (attributeValue) {
+                    _ivarName = [NSString stringWithUTF8String:attributeValue];
+                }
+            }
+        }
+        
+        _encodingType = type;
+        
+        if ([_name length]) {
+            
+            if (!_setter) {
+                _setter = NSSelectorFromString([NSString stringWithFormat:@"set%@%@",[_name substringToIndex:1].uppercaseString,[_name substringFromIndex:1]]);
+            }
+            
+            if (!_getter) {
+                _getter = NSSelectorFromString(_name);
+            }
+        }
+    }
     return self;
 }
 
@@ -136,5 +310,160 @@ JOEncodingType JOEncodingGetType(const char *encodingType) {
 #pragma mark - Class Info
 
 @implementation JOClassInfo
+
+- (instancetype)initWithClass:(Class)cls {
+
+    if (!cls) {
+        return nil;
+    }
+    
+    self  = [super init];
+    if (self) {
+        
+        _cls = cls;
+        _superCls = class_getSuperclass(cls);
+        _isMeta = class_isMetaClass(cls);
+        if (!_isMeta) {
+            _metaCls = objc_getMetaClass(class_getName(cls));
+        }
+        _name = NSStringFromClass(cls);
+        [self updateClassInfo];
+        
+        _superClassInfo = [JOGetClass(self) classInfoWithClass:_superCls];
+    }
+    
+    return self;
+}
+
+- (void)updateClassInfo {
+
+    _protocols = [NSArray array];
+    _ivarInfos = [NSDictionary dictionary];
+    _methodInfos = [NSDictionary dictionary];
+    _propertyInfos = [NSDictionary dictionary];
+    
+    //protocols
+    uint protocolCount;
+    Protocol * __unsafe_unretained *protocolList = class_copyProtocolList(_cls, &protocolCount);
+    if (protocolList) {
+        NSMutableArray *protocols = [NSMutableArray array];
+        for (uint i = 0; i < protocolCount; i++) {
+            const char *protocol = protocol_getName(protocolList[i]);
+            if (protocol) {
+                [protocols addObject:[NSString stringWithUTF8String:protocol]];
+            }
+        }
+        _protocols = [protocols copy];
+        free(protocolList);
+    }
+    
+    //ivar info dic
+    uint ivarCount;
+    Ivar *ivarList = class_copyIvarList(_cls, &ivarCount);
+    
+    if (ivarList) {
+        
+        NSMutableDictionary *ivarInfos = [NSMutableDictionary dictionary];
+        for (uint i = 0; i < ivarCount; i++) {
+            JOClassIvarInfo *ivarInfo = [[JOClassIvarInfo alloc] initWithIvar:ivarList[i]];
+            if (ivarInfo && ivarInfo.name) {
+                [ivarInfos setObject:ivarInfo forKey:ivarInfo.name];
+            }
+        }
+        _ivarInfos = [ivarInfos copy];
+        free(ivarList);
+    }
+    
+    //method info dic
+    uint methodCount;
+    Method *methodList = class_copyMethodList(_cls, &methodCount);
+    
+    if (methodList) {
+        
+        NSMutableDictionary *methodInfos = [NSMutableDictionary dictionary];
+        for (uint i = 0; i< methodCount; i++) {
+            JOClassMethodInfo *methodInfo = [[JOClassMethodInfo alloc] initWithMethod:methodList[i]];
+            if (methodInfo && methodInfo.name) {
+                [methodInfos setObject:methodInfo forKey:methodInfo.name];
+            }
+        }
+        _methodInfos = [methodInfos copy];
+        free(methodList);
+    }
+    
+    //property info dic
+    uint propertyCount;
+    objc_property_t *propertyList = class_copyPropertyList(_cls, &propertyCount);
+    
+    if (propertyList) {
+        
+        NSMutableDictionary *propertyInfos = [NSMutableDictionary dictionary];
+        for (uint i = 0; i < propertyCount; i++) {
+            JOClassPropertyInfo *propertyInfo = [[JOClassPropertyInfo alloc] initWithProperty:propertyList[i]];
+            if (propertyInfo && propertyInfo.name) {
+                [propertyInfos setObject:propertyInfo forKey:propertyInfo.name];
+            }
+        }
+        _propertyInfos = [propertyInfos copy];
+        free(propertyList);
+    }
+}
+
++ (instancetype)classInfoWithClass:(Class)cls {
+
+    return [JOClassInfo classInfoWithClass:cls update:NO];
+}
+
++ (instancetype)classInfoUpdateWithClass:(Class)cls {
+
+    return [JOClassInfo classInfoWithClass:cls update:YES];
+}
+
++ (instancetype)classInfoWithClassName:(NSString *)className {
+    
+    return [JOClassInfo classInfoWithClass:NSClassFromString(className) update:NO];
+}
+
++ (instancetype)classInfoUpdateWithClassName:(NSString *)className {
+    
+    return [JOClassInfo classInfoWithClass:NSClassFromString(className) update:YES];
+}
+
++ (instancetype)classInfoWithClass:(Class)cls update:(BOOL)update{
+    
+    if (!cls) {
+        return nil;
+    }
+    
+    static CFMutableDictionaryRef classCache;
+    static CFMutableDictionaryRef metaCache;
+    static dispatch_semaphore_t lock;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        classCache = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+        metaCache = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+        lock = dispatch_semaphore_create(1);
+    });
+    
+    dispatch_semaphore_wait(lock, DISPATCH_TIME_FOREVER);
+    //先从缓存里面去寻找
+    JOClassInfo *classInfo = CFDictionaryGetValue(class_isMetaClass(cls)?metaCache:classCache, (__bridge const void *)(cls));
+    if (classInfo && update) {
+        [classInfo updateClassInfo];
+    }
+    dispatch_semaphore_signal(lock);
+    
+    if (!classInfo) {
+        classInfo = [[JOClassInfo alloc] initWithClass:cls];
+        if (classInfo) {
+            dispatch_semaphore_wait(lock, DISPATCH_TIME_FOREVER);
+            CFDictionarySetValue(class_isMetaClass(cls)?metaCache:classCache, (__bridge const void *)(cls), (__bridge const void *)(classInfo));
+            dispatch_semaphore_signal(lock);
+        }
+    }
+    
+    return classInfo;
+}
 
 @end
